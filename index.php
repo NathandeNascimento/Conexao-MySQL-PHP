6ad5f01f17806d24c803b10463bed63e8e55a464
NathandeNascimento <nathanoliv.nascimento@gmail.com>
2021-08-10 05:04:41 -0300

    Front p/ teste

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
-<??>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>PHP / MySQL</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='estilo.css'>
+ 
+</head>
+
+<body>
+<div class="corpo ">
+
+ <b>Cadastro no MySQL</b> 
+
+ 
+<form action="querecebe.php" method="post">
+    <ul>
+        <li><label>Nome:</label></li>
+        <input type="text" name="nome">
+        <li><label>Idade:</label></li>
+        <input type="text" name="idade"><br><br>
+        <input type="submit" value="Enviar" >
+    </ul>
+</form>
+</div>      
+</body>
+</html>
