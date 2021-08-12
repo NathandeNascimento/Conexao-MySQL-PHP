6d22b0abbb9d54de554212d92de7a4ee126bed5a
NathandeNascimento <nathanoliv.nascimento@gmail.com>
2021-08-12 05:53:36 -0300

    Back-end Pesquisa de Cadastro

---
+++ busca.php
@@ -0,0 +1,31 @@
+<? include_once "config.php";?>
+<? include_once "top.php";?>
+
+<html>
+    <Body>
+        <h3>Pesquisa</h3>
+
+        <Table>
+            <Th width="5%" style="text-align:lefth;">ID</Th>
+            <Th width="25%" style="text-align:lefth;">Nome</Th>
+            <Th width="25%" style="text-align:lefth;">Idade</Th>
+        </Table>
+
+        <?
+        $busca = $_GET["busca"];
+        $result_nomes = "SELECT * FROM clientes WHERE Nome LIKE '$busca%' Order By nome ASC limit 100";
+        $resultado_nomes = mysqli_query($conn, $result_nomes);
+        $conta = mysqli_num_rows($resultado_nomes);
+        while($rows_nomes = mysqli_fetch_array($resultado_nomes))
+        {
+            echo'<table>
+            <br/>
+                <td width="9%" align="left"> ' .$rows_nomes["id"] .' </a > </td>
+                <td width="15%" align="left"> ' .$rows_nomes["Nome"] .' </a > </td> 
+                <td width="5%" align="left"> ' .$rows_nomes["idade"] .'  </td> 
+            </table>';} 
+
+        ?>
+        <div><h3>Total:  <? echo $conta?></h3></div>
+    </Body>
+</html>
